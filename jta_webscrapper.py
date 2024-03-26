1f32205b051f49402cacfed245352659903148d5
shlomi.bereznik@gmail.com <shlomi.bereznik@gmail.com>
2024-03-26 14:23:52 +0200

    Implementing Wrapping functions which wraps all the process.

--- jta_webscrapper.py
+++ jta_webscrapper.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -51,3 +52,71 @@ def get_author_jta(article_page) -> str:
 def get_tags_jta(article_page) -> str:
     tag = article_page.find('div', class_="post-categories").text.strip().replace('\t', '').replace('\n', '')
     return tag
+
+def process_article(df, article_url):
+    """Process an article URL and add data to the DataFrame."""
+    logging.info(f"Scraping data from article URL: {article_url}")
+    article_page = get_html_content(article_url)
+    soup = BeautifulSoup(article_page, "html.parser")
+    if article_page is not None:
+        title = get_article_title_jta(soup)
+        content = get_full_article_jta(soup)
+        date = get_date_from_article_jta(soup)
+        tags = get_tags_jta(soup)
+        authors = get_author_jta(soup)
+        new_row = pd.DataFrame([{
+            "date": date,
+            "title": title,
+            "content": content,
+            "urls": article_url,
+            "tags": tags,
+            "authors":authors
+
+        }])
+        df = pd.concat([df, new_row], ignore_index=True)
+        print(f"Data for {article_url} added to DataFrame")
+        logging.info(f"Data for {article_url} added to DataFrame")
+        time.sleep(2)
+    else:
+        logging.warning(f"Failed to fetch content from article URL: {article_url}")
+
+    return df
+
+def fetch_all_data_jta(base_url, n, file_path='jta.csv'):
+    """Fetch data from pages and articles."""
+    df = read_existing_data(file_path)
+    generated_urls = generate_page_urls(base_url, n)
+
+    temp_df = pd.DataFrame(
+        columns=["date", "title", "content", "urls", "tags", "authors"])  # Initialize a temporary DataFrame
+
+    for url in generated_urls:
+        logging.info(f"Processing page URL: {url}")
+        print(f"Processing page URL: {url}")
+        page = get_html_content(url)
+        if page is None:
+            continue
+        articles_urls = get_articles_urls_from_page_jta(url)
+        break_outer_loop = False  # Flag to control the outer loop
+        for article_url in articles_urls:
+            print(article_url)
+            if article_url in df['urls'].values:
+                logging.info(f"Article {article_url} already processed. Stopping loop.")
+                break_outer_loop = True
+                break
+            try:
+                df = process_article(df, article_url)
+            except Exception as e:
+                logging.error(f"Error processing article {article_url}: {e}")
+        if break_outer_loop:
+            break  # Break out of outer loop
+    temp_df['date'] = pd.to_datetime(temp_df['date']).dt.strftime('%d-%b-%y')
+    # Concatenate temp_df with df to maintain the correct order
+    df = pd.concat([temp_df, df], ignore_index=True)
+
+    # Save the final DataFrame to the specified file path
+    df.to_csv(file_path, index=False, encoding='utf-8')
+
+    logging.info("Script execution completed jta ")
+
+    return df
