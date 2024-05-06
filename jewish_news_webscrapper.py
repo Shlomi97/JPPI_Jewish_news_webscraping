b08188734280f9408ab095b65ad7eb487988ef1a
shlomi.bereznik@gmail.com <shlomi.bereznik@gmail.com>
2024-05-06 10:58:31 +0300

    Creating Jewish news webscrapper file

---
+++ jewish_news_webscrapper.py
@@ -0,0 +1,12 @@
+import logging
+import time
+import pandas as pd
+from os.path import exists
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException
+from utills import get_html_content
+
